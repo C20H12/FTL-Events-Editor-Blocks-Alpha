3b4a706deadef7cc6ae6d74cc40eb56db78ab4e3
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2022-04-24 16:45:51 -0600

    changed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import {useState, useEffect} from 'react'
 import './App.css'
 
-function EventSimBox(): JSX.Element{
+function EventSimText(): JSX.Element{
   return(
-    <div className="eventBox">
-        <div className="corners">
-          <div className="main_event">
-            
-          </div>
-        </div>
-    </div>
+    <span className='text'>aaaaaaaaaaaaaaa</span>
+  )
+}
+
+function EventSimChoices(): JSX.Element {
+  return(
+    <ol>
+      <li>choice</li>
+    </ol>
+  )  
+}
+
+function EventSimMsgBox(): JSX.Element {
+  return(
+    <div className='message'>aaaaaaa</div>
   )
 }
 
 export default function App(): JSX.Element{
 
-
   return(
-    <>
-      <EventSimBox />
-    </>
+    <div className="eventBox">
+        <div className="corners">
+          <div className="main_event">
+            <EventSimText />
+            <EventSimMsgBox />
+            <EventSimChoices />
+          </div>
+        </div>
+    </div>
   )  
 }
 
