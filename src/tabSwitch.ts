ec1f1ebc4517d51f8f53cb2f33654691510cf2f6
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2022-06-21 23:08:42 -0600

    added temp fix for input areas not hiding when changing tabs

--- src/tabSwitch.ts
+++ src/tabSwitch.ts
@@ -1,7 +1,9 @@
 const blocksTabBtn = document.querySelector("#showBlocks") as HTMLDivElement;
 const eventTestTabBtn = document.querySelector("#showEvent") as HTMLDivElement;
 const eventTestTab = document.querySelector("#root") as HTMLDivElement;
-const blocksEditTab = document.querySelector("#blocksWorkspace") as HTMLDivElement;
+const blocksEditTab = document.querySelector(
+  "#blocksWorkspace"
+) as HTMLDivElement;
 
 blocksTabBtn.addEventListener("click", () => {
   eventTestTab.style.display = "none";
@@ -21,6 +23,14 @@ eventTestTabBtn.addEventListener("click", () => {
 
   blocksTabBtn.classList.toggle("tabOpen");
   eventTestTabBtn.classList.toggle("tabOpen");
-})
+
+  // bandaid fix for text input fields not hiding when tab is closed
+  const anySelectedBlockInputField = document.querySelector(
+    "body > div.blocklyWidgetDiv.geras-renderer.classic-theme"
+  ) as HTMLDivElement;
+  if (anySelectedBlockInputField != null) {
+    anySelectedBlockInputField.innerHTML = "";
+  }
+});
 
 export default blocksEditTab;
